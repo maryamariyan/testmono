6882d6d91e9981357154a5b9ca907e0c6ca1b010
Maryam Ariyan <maryam.ariyan@microsoft.com>
2017-09-06 23:20:39 +0000

    improvement

--- Program.cs
+++ Program.cs
@@ -10,27 +10,36 @@ namespace ProjInMac
 		public static void Main()
 		{
                         // no xdg: 	System.ComponentModel.Win32Exception (0x80004005): Access denied at System.Diagnostics.Process.StartWithShellExecuteEx
-			// with xdg: 	System.NullReferenceException: Object reference not set to an instance of an object
-                        string fileToOpen = "README.md";
+			// with xdg: 	px is null
+                        string fileToOpen = "README.txt";
 
                         // no xdg:	System.ComponentModel.Win32Exception (0x80004005): Cannot find the specified file
-			// with xdg:	System.NullReferenceException: Object reference not set to an instance of an object
+			// with xdg:	px is null
                         string urlToOpen = "http://www.google.com";
 
-			// no xdg:	System.InvalidOperationException: Process must exit before requested information can be determined.
+			// no xdg:	Works fine.
 			// with xdg:	Works fine
 			string appToOpen = "nano";
 
+			// when UseShellExecute is false, FileName will be applicationname and Arguments will be args for the applicationname
+			// when true, use shell exec process to open that program instead of running a process, and the FileName will be file/url/program
 			var startInfo = new ProcessStartInfo { UseShellExecute = true, FileName = urlToOpen };
 			using (var px = Process.Start(startInfo))
 			{
-				var sb = new StringBuilder();
-				sb.Append("px has exited? "+ px.HasExited + " ");
-				px.WaitForExit();
-				sb.Append("px exit code? "+ px.ExitCode + " ");
-				Console.WriteLine(sb.ToString());
+				if (px != null) 
+				{
+					var sb = new StringBuilder();
+                                	sb.Append("px has exited? "+ px.HasExited + " ");
+
+					px.WaitForExit();
+
+  	                              	sb.Append("px exit code? "+ px.ExitCode + " ");
+        	                        Console.WriteLine(sb.ToString());
+
+				}
+				else
+					Console.WriteLine("px is null");
 			}
-			Console.ReadLine();
 		}
 	}
 }
